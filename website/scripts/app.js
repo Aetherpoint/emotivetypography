4cc2e5225b375d174aff13375a9bfaa4a450c81a
Andrew Johnson <aetherpoint@gmail.com>
2017-11-05 21:20:00 -0500

    Gets toggling working

--- website/scripts/app.js
+++ website/scripts/app.js
@@ -7,17 +7,45 @@ function manageSlideState() {
 
 function advanceSlide() {
 	currentSlide++;
+	updateSlides();
 }
 function backupSlide() {
 	currentSlide--;
+	updateSlides();
 }
 
 
 function hideSlide(slideElement) {
+
+	// console.log("Hiding " + slideElement);
 	slideElement.style.display = "none";
 }
+
 function showSlide(slideElement) {
+
+	// console.log("Showing " + slideElement);
 	slideElement.style.display = "flex";
+}
+
+// Update the slides
+function updateSlides() {
+	var sliderComponent = document.getElementById('slide-container');
+	var slides = sliderComponent.querySelectorAll("#slide-container > div");
+
+	for (var i = 0; i < slides.length; i++) {
+		
+		// Hide and show the current slide
+		if (i === currentSlide) {
+			showSlide(slides[i]);
+
+			console.log(slides[i]);
+		}
+		else {
+			hideSlide(slides[i]);
+
+			console.log(slides[i]);
+		}
+	}
 }
 
 
@@ -27,7 +55,6 @@ function prepSlides() {
 	var slides = sliderComponent.querySelectorAll("#slide-container > div");
 
 	for (var i = 0; i < slides.length; i++) {
-		
 		
 		// Hide and show the current slide
 		if (i === currentSlide) {
@@ -44,19 +71,13 @@ function prepSlides() {
 			// slides[i].querySelectorAll('.button-left')
 
 			slides[i].querySelectorAll('.button-left')[0].addEventListener("click", function(event) {
-			    //do something
-
-			    console.log("HEYYYY");
+			    backupSlide();
 			});
-
-			console.log(slides[i].querySelectorAll('.button-left')[0]);
 		}
 		else if (slides[i].querySelectorAll('.button-right')[0]) {
 
 			slides[i].querySelectorAll('.button-right')[0].addEventListener("click", function(event) {
-			    //do something
-
-			    console.log("HEYYYY");
+			    advanceSlide();
 			});
 		}
 
