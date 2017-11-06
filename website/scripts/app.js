dbc8fac42fdb1bf91b57f864b7e4ec8829a36622
Andrew Johnson <aetherpoint@gmail.com>
2017-11-05 23:50:00 -0500

    Adds better logic for advancing slides

--- website/scripts/app.js
+++ website/scripts/app.js
@@ -38,12 +38,12 @@ function updateSlides() {
 		if (i === currentSlide) {
 			showSlide(slides[i]);
 
-			console.log(slides[i]);
+			// console.log(slides[i]);
 		}
 		else {
 			hideSlide(slides[i]);
 
-			console.log(slides[i]);
+			// console.log(slides[i]);
 		}
 	}
 }
@@ -54,41 +54,41 @@ function prepSlides() {
 	var sliderComponent = document.getElementById('slide-container');
 	var slides = sliderComponent.querySelectorAll("#slide-container > div");
 
-	for (var i = 0; i < slides.length; i++) {
-		
-		// Hide and show the current slide
-		if (i === currentSlide) {
-			showSlide(slides[i]);
-		}
-		else {
-			hideSlide(slides[i]);
-		}
+	// console.log(slides.length);
 
+	for (var i = 0; i < slides.length; i++) {
+
+		console.log(i);
+		
 		// Attack event listeners to the slide buttons
 
 		// If the slide has a button to advance, then add an event listener
 		if (slides[i].querySelectorAll('.button-left')[0]) {
 			// slides[i].querySelectorAll('.button-left')
 
+			console.log(slides[i].querySelectorAll('.button-left')[0]);
+
 			slides[i].querySelectorAll('.button-left')[0].addEventListener("click", function(event) {
+			    
+			    console.log("Hit Left");
 			    backupSlide();
 			});
 		}
-		else if (slides[i].querySelectorAll('.button-right')[0]) {
+		if (slides[i].querySelectorAll('.button-right')[0]) {
+
+			console.log(slides[i].querySelectorAll('.button-right')[0]);
 
 			slides[i].querySelectorAll('.button-right')[0].addEventListener("click", function(event) {
+			    
+				console.log("Hit Right");
 			    advanceSlide();
 			});
 		}
 
-
 		// console.log(slides[i].querySelectorAll('.button-right'));
 		// slides[i].style.display = "flex";
 	}
-
-
-	
 }
 
-
 prepSlides();
+updateSlides();
